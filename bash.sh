98ef21e412dcadbd2452ce14426b55a9db28f022
Iury Bernardes <bernardes.iury@gmail.com>
2025-05-11 12:48:09 -0300

    Fix bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 # This command is required to create a repository in the project.
 # Change the *path* to the minigit path
 
-alias minigit="pwd | node /path/minigit/src/cli.cjs"
+export alias minigit="pwd | node /path/minigit/src/cli.cjs"
